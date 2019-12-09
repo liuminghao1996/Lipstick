d9335e29470c2f7c7c49aed3a84d8108a831e52a
刘铭浩 <liuminghao@tuzhan.com>
2019-12-09 23:26:15 +0800

    更新

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
