fbd74b2ddedf65a7de8e1ca7660e42fd442c5d4a
刘铭浩 <liuminghao@tuzhan.com>
2019-12-10 23:33:13 +0800

    “更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,38 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'index.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase:path.resolve(__dirname, 'dist'),
+        compress:true,//gzip压缩
+        port: 9000,
+        open:true
+    },
+    module:{
+        rules: [
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.(png|jpg|jpeg|svg|gif)$/,
+                use:['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title:'口红机',
+            inject:'body',
+            minify:true
+        })
+    ]
 };
